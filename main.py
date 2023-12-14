edcf4d4f6e644ed80cbf9359caa446954613f39f
jasonmccauley <jsonmccauley@gmail.com>
2023-12-14 01:20:56 -0500

    added support for local multiplayer. imported os library to clear terminal after player 1. imported time library to add small delays between outputs. also made user selections more visually appealing.

--- main.py
+++ main.py
@@ -1,68 +1,110 @@
 import random
 from colorama import Fore,Back,Style
+import time
+import os
 
-
-
-print("Welcome to Wordle Unlimited By Jason and Omar")
+print("Welcome to...")
+time.sleep(2)
+print("""
+ __      __            .__       .___.__            ____ ___      .__  .__        .__  __             .___
+/  \    /  \___________|  |    __| _/|  |   ____   |    |   \____ |  | |__| _____ |__|/  |_  ____   __| _/
+\   \/\/   /  _ \_  __ \  |   / __ | |  | _/ __ \  |    |   /    \|  | |  |/     \|  \   __\/ __ \ / __ | 
+ \        (  <_> )  | \/  |__/ /_/ | |  |_\  ___/  |    |  /   |  \  |_|  |  Y Y  \  ||  | \  ___// /_/ | 
+  \__/\  / \____/|__|  |____/\____ | |____/\___  > |______/|___|  /____/__|__|_|  /__||__|  \___  >____ | 
+       \/                         \/           \/               \/              \/              \/     \/ 
+      """)
+time.sleep(2)
+print("by Omar Elkhafif and Jason McCauley")
+time.sleep(2)
 
 def main():
     keepGoing = True
     while keepGoing:
         while True:
-            choice = input("Which word length would you like to play? Enter 4 for 4 letter words, 5 for five letter words, or 6 for 6 letter words:\n")
+            choice = input("Which word length would you like to play? Enter the corresponding number.\n (4) Four Letter Words\n (5) Five Letter Words\n (6) Six Letter Words\n")
             try:
                 choice = int(choice)
                 if choice not in [4, 5, 6]:
-                    print("Sorry, this is not a valid game mode. Please try again")
+                    print("Sorry, the number you entered is not a valid game mode. Please try again.")
+                    time.sleep(2)
                     continue
                 break
             except ValueError:
-                print("Please enter a number.")
+                print("Please enter a valid number.")
+                time.sleep(2)
 
-       
-        word_file = open(str(choice) + '_letter_word.txt', 'r') # Accessing txt file with all 5 letter words
-        with open(str(choice) + '_letter_word.txt', 'r') as word_file:  # Using with statement for better file handling
+        word_file = open(str(choice) + '_letter_word.txt', 'r') # accessing txt file with all 'choice' letter words
+        with open(str(choice) + '_letter_word.txt', 'r') as word_file:  # using with statement for better file handling
             words = [line.strip() for line in word_file.readlines()]
-        gameLogic(choice,words)
-        
-        again = input("Play again? Put 'y' for yes or anything else for no:\n")        
-        if again == 'y':
-            keepGoing = True
-        else:
-            keepGoing = False
-            return
-
-def gameLogic(choice,words):
- 
         random_word = random.choice(words)
 
+        while True:
+            multiplayer = input("Would you like to play local multiplayer?\n (y) Yes\n (n) No\n")
+            if multiplayer == 'y':
+                print("Player one, get ready. It's your turn!")
+                time.sleep(2)
+                gameLogic(choice, words, random_word)
+                time.sleep(5)
+                os.system('cls')
 
-        print("You will now be prompted to guess the " + str(choice) + " letter word chosen. Get ready!")
+                print("Player two, get ready. It's now your turn!")
+                time.sleep(2)
+                gameLogic(choice, words, random_word)
+                break
+            elif multiplayer == 'n':
+                gameLogic(choice, words, random_word)
+                break
+            else:
+                print("Please enter a valid choice.")
+                time.sleep(2)
+                continue
+    
+        while True:
+            again = input("Would you like to play again?\n (y) Yes\n (n) No\n")        
+            if again == 'y':
+                keepGoing = True
+                break
+            elif again == 'n':
+                keepGoing = False
+                break
+            else:
+                print("Please enter a valid choice.")
+                time.sleep(2)
+                continue
+
+def gameLogic(choice, words, random_word):
+        print("You will now be prompted to guess the chosen " + str(choice) + " letter word. Good luck!")
         print("_ " * choice)
         guess_counter = 0
+        time.sleep(3)
+        print("Please enter your guess for the word. Remember this must be a "+ str(choice) + " letter word that exists in the English Dictionary.")
+        time.sleep(2)
+
         while guess_counter < (choice + 1):
             guess_counter += 1
             while True:
-                guess = input("Please enter your guess for the word. Remember this must be a "+ str(choice) + " letter word that exists in the English Dictionary! This is guess number " + str(guess_counter) + "\n").lower()
-
-            
+                guess = input("This is guess number " + str(guess_counter) + ".\n").lower()
+ 
                 if len(guess) != choice:
-                    print("Your guess must be exactly "+ str(choice) + " letters. Try again.")
+                    print("Remember, your guess must be exactly "+ str(choice) + " letters. Please try again.")
                     continue
 
                 if guess not in words:
-                    print("Your guess is not a valid English word or not in the list. Try again.")
+                    print("Your guess is either not a valid English word or not in the list. Please try again.")
                     continue
-
                 break
             
             if guess == random_word:
                 print(Back.GREEN + random_word + Style.RESET_ALL)
-                print("You got it")
+                print("Congratulations! You correctly guessed the word!")
+                time.sleep(2)
                 break
             elif guess_counter == choice + 1:
-                print("Sorry you did not get it!")
-                print("The word was", random_word)
+                print("Sorry, you did not guess the word in time!")
+                time.sleep(2)
+                print('The word was "' + str(random_word) + '".')
+                time.sleep(2)
+                break
             else:
                 modified_guess = []
                 matched_indices = set()
@@ -88,7 +130,6 @@ def gameLogic(choice,words):
 
                 modified_guess_str = ''.join(modified_guess)
                 print(modified_guess_str)
-
 
         return   
 main()
